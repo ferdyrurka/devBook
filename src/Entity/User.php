140153e363c22747d11cfcffa94b683b221cbfbc
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-09-03 20:14:01 +0200

    Added length to id in UserEntity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -21,7 +21,7 @@ class User implements UserInterface, \Serializable
     /**
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
-     * @ORM\Column(type="integer")
+     * @ORM\Column(type="integer", length=11)
      */
     private $id;
 
