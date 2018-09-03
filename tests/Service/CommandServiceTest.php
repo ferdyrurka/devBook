f4c247382a09a0defcba31f59da74b57414e6de7
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-09-03 11:31:52 +0200

    Refactoring CommandService TDD

--- tests/Service/CommandServiceTest.php
+++ tests/Service/CommandServiceTest.php
@@ -1,6 +1,9 @@
 <?php
 declare(strict_types=1);
 
+namespace App\Tests\Service;
+
+use App\Command\CommandInterface;
 use App\Service\CommandService;
 use PHPUnit\Framework\TestCase;
 
@@ -10,18 +13,20 @@ use PHPUnit\Framework\TestCase;
 class CommandServiceTest extends TestCase
 {
 
+    private $commandService;
+
     public function setUp()
     {
+        $this->commandService = new CommandService();
         parent::setUp();
-    }
-
-    public function testSetCommand()
-    {
-
     }
 
     public function testExecute()
     {
+        $command = \Mockery::mock(CommandInterface::class);
+        $command->shouldReceive('execute')->once();
 
+        $this->assertInstanceOf(CommandService::class, $this->commandService->setCommand($command));
+        $this->assertNull($this->commandService->execute());
     }
 }
