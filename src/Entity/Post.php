25f6c3ab8b1159d4f233b3af3f3bb0149bfc3e16
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-09-03 20:21:13 +0200

    Delete getter and setter

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -71,22 +71,6 @@ class Post
     }
 
     /**
-     * @return int
-     */
-    public function getUserId(): int
-    {
-        return $this->userId;
-    }
-
-    /**
-     * @param int $userId
-     */
-    public function setUserId(int $userId): void
-    {
-        $this->userId = $userId;
-    }
-
-    /**
      * @return \DateTime
      */
     public function getCreatedAt(): \DateTime
