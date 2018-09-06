da24e54d755d79ddbdae5901894c3ca8a51c4cba
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-09-06 21:32:34 +0200

    PostWorker console commands added posts in RabbitMQ queue

--- src/Console/RabbitMQ/PostWorkerConsole.php
+++ src/Console/RabbitMQ/PostWorkerConsole.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace App\Console\RabbitMQ;
 
-use App\Command\Console\AddPostCommand;
+use App\Console\RabbitMQ\Command\AddPostCommand;
 use App\Service\RabbitMQConnectService;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -49,11 +49,25 @@ class PostWorkerConsole extends Command
 
         $channel->queue_declare('post', false, false, false);
 
-        $channel->basic_consume('post', false, false, false, [$this->addPostCommand, 'execute']);
+        $channel->basic_consume(
+            'post',
+            '',
+            false,
+            true,
+            false,
+            false,
+            [$this->addPostCommand, 'execute']
+        );
+
+        $output->writeln('I\'m ready to working!');
 
         while (count($channel->callbacks)) {
             $channel->wait();
+
+            $output->writeln('I\'m added post to database!');
         }
+
+        $output->writeln('Connection closed, bye. :(');
 
         $service->close($channel);
 
