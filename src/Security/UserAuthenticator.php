bc5da75ac9dd5dbd93e36427ee1e72c4e77a8220
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-09-06 21:35:04 +0200

    Added UserAuthenticator

---
+++ src/Security/UserAuthenticator.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+/**
+ * Class UserAuthenticator
+ * @package App\Security
+ */
+class UserAuthenticator extends AbstractGuardAuthenticator
+{
+
+    /**
+     * @param Request $request
+     * @return bool
+     */
+    public function supports(Request $request): bool
+    {
+        return $request->headers->has('X-AUTH-TOKEN');
+    }
+
+    /**
+     * @param Request $request
+     * @return array
+     */
+    public function getCredentials(Request $request): array
+    {
+        return [
+            'token' => $request->headers->get('X-AUTH-TOKEN'),
+        ];
+    }
+
+    /**
+     * @param mixed $credentials
+     * @param UserProviderInterface $userProvider
+     * @return null|UserInterface|void
+     */
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        $apiKey = $credentials['token'];
+
+        if (null === $apiKey) {
+            return;
+        }
+
+        return $userProvider->loadUserByUsername($apiKey);
+    }
+
+    /**
+     * @param mixed $credentials
+     * @param UserInterface $user
+     * @return bool
+     */
+    public function checkCredentials($credentials, UserInterface $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * @param Request $request
+     * @param TokenInterface $token
+     * @param string $providerKey
+     * @return null|Response
+     */
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey): ?Response
+    {
+        return null;
+    }
+
+    /**
+     * @param Request $request
+     * @param AuthenticationException $exception
+     * @return JsonResponse
+     */
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): JsonResponse
+    {
+        $data = [
+            'message' => strtr($exception->getMessageKey(), $exception->getMessageData())
+        ];
+
+        return new JsonResponse($data, Response::HTTP_FORBIDDEN);
+    }
+
+    /**
+     * @param Request $request
+     * @param AuthenticationException|null $authException
+     * @return Response
+     */
+    public function start(Request $request, AuthenticationException $authException = null): Response
+    {
+        return new Response('Authentication Required', Response::HTTP_UNAUTHORIZED);
+    }
+
+    /**
+     * @return bool
+     */
+    public function supportsRememberMe(): bool
+    {
+        return true;
+    }
+}
