d695d35217bc1c779110f47e6549dcf40b25191b
user <ferdyrurka@ubuntu.ubuntu-domain>
2018-10-01 22:01:45 +0200

    ConversationRepository added method getByConversationId

--- web/src/Repository/ConversationRepository.php
+++ web/src/Repository/ConversationRepository.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Repository;
 
 use App\Entity\Conversation;
+use App\Exception\ConversationNotExistException;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
 
@@ -20,5 +21,23 @@ class ConversationRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Conversation::class);
+    }
+
+    /**
+     * @param string $conversationId
+     * @return Conversation
+     * @throws ConversationNotExistException
+     */
+    public function getByConversationId(string $conversationId): Conversation
+    {
+        $conversation = $this->findBy(['conversationId' => $conversationId]);
+
+        if (empty($conversation)) {
+            throw new ConversationNotExistException(
+                'Conversation by conversationId: ' . $conversationId . 'not found!'
+            );
+        }
+
+        return $conversation;
     }
 }
