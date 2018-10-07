d86c3feb6d621ab8f4e333e539a7abfa62201ec5
user <ferdyrurka@ubuntu.ubuntu-domain>
2018-10-07 18:24:07 +0200

    CreateConversationCommand TDD corrected to redis

--- web/tests/Command/Console/DevMessenger/CreateConversationCommandTest.php
+++ web/tests/Command/Console/DevMessenger/CreateConversationCommandTest.php
@@ -9,11 +9,12 @@ use App\Entity\User;
 use App\Entity\UserToken;
 use App\Exception\ConversationExistException;
 use App\Exception\InvalidException;
-use App\Repository\ConversationRepository;
 use App\Repository\UserRepository;
+use App\Service\RedisService;
 use Doctrine\ORM\EntityManagerInterface;
 use PHPUnit\Framework\TestCase;
 use \Mockery;
+use Predis\Client;
 
 /**
  * Class CreateConversationCommandTest
@@ -21,26 +22,48 @@ use \Mockery;
  */
 class CreateConversationCommandTest extends TestCase
 {
+    use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+
     /**
      * @throws \Exception
      */
     public function testExecute()
     {
+        $client = Mockery::mock(Client::class);
+        $client->shouldReceive('set')->once();
+
+        $redisService = Mockery::mock(RedisService::class);
+        $redisService->shouldReceive('setDatabase')->withArgs([2])->andReturn($client);
+
         $entityManager = Mockery::mock(EntityManagerInterface::class);
         $entityManager->shouldReceive('persist')->once()->withArgs([Conversation::class]);
         $entityManager->shouldReceive('flush')->once();
 
-        $userToken = Mockery::mock(UserToken::class);
-        $userToken->shouldReceive('getPrivateWebToken')->once()->andReturn('receive_private_user_token');
+        #User receive
+
+        $userTokenReceive = Mockery::mock(UserToken::class);
+        $userTokenReceive->shouldReceive('getPrivateWebToken')->once()->andReturn('receive_private_user_token');
+        $userTokenReceive->shouldReceive('getPrivateMobileToken')->once()
+            ->andReturn('receive_private_mobile_user_token');
 
         $userReceive = Mockery::mock(User::class);
         $userReceive->shouldReceive('getFirstName')->once()->andReturn('FirstName');
         $userReceive->shouldReceive('getSurname')->once()->andReturn('Surname');
-        $userReceive->shouldReceive('getId')->times(2)->andReturn(2);
-        $userReceive->shouldReceive('getUserTokenReferences')->once()->andReturn($userToken);
+        $userReceive->shouldReceive('getId')->andReturn(2);
+        $userReceive->shouldReceive('getUserTokenReferences')->once()->andReturn($userTokenReceive);
+
+        #Send
+
+        $userTokenSend = Mockery::mock(UserToken::class);
+        $userTokenSend->shouldReceive('getPrivateWebToken')->once()->andReturn('send_private_user_token');
+        $userTokenSend->shouldReceive('getPrivateMobileToken')->once()
+            ->andReturn('send_private_mobile_user_token');
 
         $userSend = Mockery::mock(User::class);
-        $userSend->shouldReceive('getId')->times(2)->andReturn(1);
+        $userSend->shouldReceive('getId')->andReturn(1);
+        $userSend->shouldReceive('getUserTokenReferences')->once()->andReturn($userTokenSend);
+
+        #User Repository
 
         $userRepository = Mockery::mock(UserRepository::class);
         $userRepository->shouldReceive('getOneByPublicToken')->withArgs(['receive_user_token'])
@@ -51,7 +74,8 @@ class CreateConversationCommandTest extends TestCase
 
         $createConversationCommand = new CreateConversationCommand(
             $entityManager,
-            $userRepository
+            $userRepository,
+            $redisService
         );
         $createConversationCommand->setSendUserToken('send_user_token');
         $createConversationCommand->setReceiveUserToken('receive_user_token');
@@ -63,8 +87,6 @@ class CreateConversationCommandTest extends TestCase
         $this->assertNotNull($result['conversationId']);
         $this->assertTrue($result['result']);
         $this->assertEquals('FirstName Surname', $result['fullName']);
-        $this->assertEquals('send_user_token', $result['usersId'][0]);
-        $this->assertEquals('receive_private_user_token', $result['usersId'][1]);
 
         $this->conversationExistExceptionTest($createConversationCommand);
     }
@@ -88,10 +110,14 @@ class CreateConversationCommandTest extends TestCase
     {
         $entityManager = Mockery::mock(EntityManagerInterface::class);
         $userRepository = Mockery::mock(UserRepository::class);
+        $redisService = Mockery::mock(RedisService::class);
+        $client = Mockery::mock(Client::class);
+        $redisService->shouldReceive('setDatabase')->withArgs([2])->andReturn($client);
 
         $createConversationCommand = new CreateConversationCommand(
             $entityManager,
-            $userRepository
+            $userRepository,
+            $redisService
         );
 
         $createConversationCommand->setSendUserToken('send_user_token');
