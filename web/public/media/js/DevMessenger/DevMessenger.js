fa1f5abd4f6e65b5b027840088ae0d62b09d4a52
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-10-10 22:06:23 +0200

    DevMessenger js

--- web/public/media/js/DevMessenger/DevMessenger.js
+++ web/public/media/js/DevMessenger/DevMessenger.js
@@ -1,3 +1,7 @@
+/**
+ * Responsible class for search friends by phrase. To search friends using api website to url: /api/search-friends?q=
+ * Set result in id "results-search-user"
+ */
 class SearchFriends
 {
     search(phrase)
@@ -16,31 +20,69 @@ class SearchFriends
                     + friend['fullName'] + '</span></div>';
             });
 
-            $(".results-search-user").html(result);
+            $("#results-search-user").html(result);
         }).fail(function () {
             console.log('Error API no working. Please a contact from administrator.');
         });
     }
 }
 
+/**
+ *  Set a history conversation by conversation id. Using ajax request to website url: api/get-messages/
+ *  Set history in id "conversation" and scroll to down messages.
+ */
 class HistoryConversation
 {
+    /**
+     * @param conversationId
+     * offset is 1 because always set first history in other class.
+     */
     constructor(conversationId)
     {
-        this.coversationId = conversationId;
+        this.conversationId = conversationId;
+        this.offset = 1;
+        this.block = false;
     }
 
-    setHistory(offset)
+    /**
+     * @param conversationId
+     */
+    setConversationId(conversationId)
     {
+        this.conversationId = conversationId;
+    }
+
+    /**
+     * @return string
+     */
+    getConversationId()
+    {
+        return this.conversationId;
+    }
+
+    /**
+     * @param offset
+     * @param append
+     * @return boolean
+     * return value to this block
+     */
+    setHistory(offset, append)
+    {
+        var thisHistory = this;
+
         $.ajax({
-            url: 'api/get-messages/' + this.coversationId + '/' + offset,
+            url: 'api/get-messages/' + this.conversationId + '/' + offset,
             method:"GET"
         }).done(function (res) {
             var messages = '';
 
+            if (res[0].length === 0) {
+                thisHistory.blockUpdate();
+            }
+
             res[0].forEach(function (message) {
                 if (message['template'] === 'From') {
-                    messages = messages +
+                    messages =
                         '<div>' +
                         '   <div class="time-message">' +
                         '       <span>' + message['date'] + '</span>' +
@@ -48,9 +90,10 @@ class HistoryConversation
                         '   <div class="me-message">' +
                         '       <span class="haze-message">' + message['message'] + '</span>' +
                         '   </div>' +
-                        '</div>';
+                        '</div>'
+                        + messages;
                 } else {
-                    messages = messages +
+                    messages =
                         '<div>' +
                         '   <div class="time-message">' +
                         '       <span>' + message['date'] + '</span>' +
@@ -58,21 +101,76 @@ class HistoryConversation
                         '   <div class="contact-message">' +
                         '       <span class="haze-message">' + message['message'] + '</span>' +
                         '   </div>' +
-                        '</div>';
+                        '</div>'
+                        + messages;
                 }
             });
 
-            $("#conversation").html(messages);
-            $("#conversation").animate({ scrollTop: $(document).height() }, "fast");
+            if (append === true) {
+                $("#conversation").prepend(messages);
+            } else {
+                $("#conversation").html(messages);
+                $("#conversation").animate({ scrollTop: $(document).height() }, "fast");
+            }
         }).fail(function () {
             $("#conversation").html("Please refresh a page!");
             console.log('Error, doesn\'t connection a API url. Please a contact from administrator.')
-        })
+        });
+    }
+
+    /**
+     *
+     */
+    updateHistory()
+    {
+        if (this.block === true) {
+            console.log('test');
+            return;
+        }
+
+        this.setHistory(this.offset, true);
+        ++this.offset;
+    }
+
+    /**
+     * Blocked update (send ajax request)
+     */
+    blockUpdate()
+    {
+        this.block = true;
+    }
+
+    /**
+     * Unblocked update history
+     */
+    unblock()
+    {
+        this.block = false;
+    }
+
+    /**
+     * @param offset
+     */
+    setOffset(offset)
+    {
+        this.offset = offset;
     }
 }
 
-class ListConversation
+/**
+ * Set List user conversation. In list is located full name user and last message.
+ * List is loading in
+ */
+class Conversation
 {
+    /**
+     *
+     */
+    constructor()
+    {
+       this.conversationId = '';
+    }
+
     setListConversation()
     {
         $.ajax({
@@ -84,6 +182,7 @@ class ListConversation
 
             if (res.length === 0) {
                 //Messages is empty
+
                 $("#conversation").html('');
             } else {
                 res.forEach(function (conversation) {
@@ -93,22 +192,23 @@ class ListConversation
                         $("#name-message").html(conversation['fullName']);
                         $("#conversationId").val(conversation['conversationId']);
 
-                        var getMessages = new HistoryConversation(conversation['conversationId']);
-                        getMessages.setHistory(1);
+                        var historyConversation = new HistoryConversation(conversation['conversationId']);
+                        historyConversation.setHistory(0, false);
                     }
 
                     conversations = conversations +
-                        '<div class="your-conversation" conversationId="' + conversation['conversationId'] + '">' +
+                        '<div class="your-conversation" fullName="' + conversation['fullName'] + '" id="' + conversation['conversationId'] + '">' +
                         '   <div class="name">\n' +
                         '       <span>' + conversation['fullName'] + '</span>' +
                         '   </div>' +
                         '    <div class="last-message">\n' +
-                        conversation['lastMessage'] +
+                                conversation['lastMessage'] +
                         '    </div>' +
+                            '<div class="new-message"></div>'+
                         '</div>';
                 });
 
-                $("#all-conversations").append(conversations);
+                $("#list-conversations").html(conversations);
             }
 
             $("#load-conversation").remove();
@@ -117,21 +217,117 @@ class ListConversation
             console.log('Error, doesn\'t connection a API url. Please a contact from administrator.')
         })
     }
+
+    setConversation(conversationId, fullName)
+    {
+        $("#conversationId").val(conversationId);
+
+        var historyConversation = new HistoryConversation(conversationId);
+        historyConversation.setHistory(0, false);
+
+        $("#name-message").show("slow").html(fullName);
+        $("#" + conversationId + " > .new-message").html('');
+    }
 }
 
+/**
+ * Connect with DevMessenger WebSocket.
+ * His tasks a registry user (online), send messages, receive messages and create conversation.
+ */
 class DevMessenger
 {
+    /**
+     * @param userId
+     */
     constructor(userId)
     {
-        this.conn = new WebSocket('ws://127.0.0.6:2013');
+        this.conn = new WebSocket('ws://localhost:2013');
 
-        this.offset = 0;
         this.userId = userId;
         this.registry(this.userId);
 
         this.eventMessages();
     }
 
+    eventMessages()
+    {
+        this.conn.addEventListener('message', function (event) {
+            var msg = JSON.parse(event.data);
+
+            switch (msg['type']) {
+                case 'message' :
+                    if (msg['conversationId'] !== $("#conversationId").val()) {
+                        $("#" + msg['conversationId'] + " > .new-message").prepend('<i class="fas fa-bell"></i>');
+
+                        break;
+                    }
+
+                    var date = new Date();
+                    let mounth = date.getMonth() + 1;
+
+                    $("#conversation").append(
+                        '<div>' +
+                        '   <div class="time-message">' +
+                        '       <span>' + date.getFullYear() + '-' + mounth + '-' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + '</span>' +
+                        '   </div>' +
+                        '   <div class="contact-message">' +
+                        '       <span class="haze-message">' + msg['message'] + '</span>' +
+                        '   </div>' +
+                        '</div>'
+                    );
+
+                    break;
+                case 'create' :
+                    if (msg['result'] === true) {
+                        $("#conversationId").val(msg['conversationId']);
+
+                        $("#list-conversations").prepend(
+                            '<div class="your-conversation">\n' +
+                            '     <div class="name">\n' +
+                            '         <span>' + msg['fullName'] + '</span>\n' +
+                            '     </div>\n' +
+                            '     <div class="last-message"></div>\n' +
+                            '</div>'
+                        );
+                        $("#conversation").html('');
+
+                        $("#name-message").text(msg['fullName']);
+                    } else if (msg['result'] === false) {
+                        $("#conversation-exist-alert").removeClass("hidden");
+                    }
+
+                    break;
+                default:
+
+                    /**
+                     * Undefined type send.
+                     */
+                    console.log('API not working. Please contact from Administrator.');
+                    break;
+            }
+        });
+    }
+
+    /**
+     * @param message
+     */
+    addMeMessage(message)
+    {
+        $("#conversation").append(
+            '<div>' +
+            '   <div class="time-message">' +
+            '       <span>' + new Date().toLocaleString() + '</span>' +
+            '   </div>' +
+            '   <div class="me-message">' +
+            '       <span class="haze-message">' + message + '</span>' +
+            '   </div>' +
+            '</div>'
+        );
+    }
+
+    /**
+     * @param userId
+     */
     registry(userId)
     {
         this.conn.onopen = () => this.conn.send(
@@ -142,6 +338,10 @@ class DevMessenger
         );
     }
 
+    /**
+     * @param message
+     * @param conversationId
+     */
     sendMessage(message, conversationId)
     {
         this.conn.send(
@@ -158,67 +358,9 @@ class DevMessenger
         $(".send-message-input").val('');
     }
 
-    eventMessages()
-    {
-        this.conn.addEventListener('message', function (event) {
-            var msg = event.data;
-            var type = event.data['type'];
-            console.log(event.data);
-
-            if (type === 'message') {
-                $(".conversation").append(
-                    '<div>' +
-                    '   <div class="time-message">' +
-                    '       <span>' + new Date().toLocaleString() + '</span>' +
-                    '   </div>' +
-                    '   <div class="contact-message">' +
-                    '       <span class="haze-message">' + event.data + '</span>' +
-                    '   </div>' +
-                    '</div>'
-                );
-            } else if (type === 'create') {
-                console.log('type');
-                if (msg['result'] === 'true') {
-                    this.offset = 0;
-
-                    $("#conversationId").val(msg['conversationId']);
-
-                    $("#all-conversations").appendTo('' +
-                        '<div class="your-conversation">\n' +
-                        '     <div class="name">\n' +
-                        '         <span>' + msg['fullName'] + '</span>\n' +
-                        '     </div>\n' +
-                        '     <div class="last-message"></div>\n' +
-                        '</div>'
-                    );
-                    $(".conversation").html('');
-
-                    $("#name-message").text(msg['fullName']);
-                } else if (msg['result'] === true) {
-                    this.offset = 0;
-                    var historyConversation = new HistoryConversation(msg['conversationId']);
-                    historyConversation.setHistory(this.offset);
-                    ++this.offset;
-                }
-            }
-            console.log('API not working. Please contact from Administrator.');
-        });
-    }
-
-    addMeMessage(message)
-    {
-        $(".conversation").append(
-            '<div>' +
-            '   <div class="time-message">' +
-            '       <span>' + new Date().toLocaleString() + '</span>' +
-            '   </div>' +
-            '   <div class="me-message">' +
-            '       <span class="haze-message">' + message + '</span>' +
-            '   </div>' +
-            '</div>'
-        );
-    }
-
+    /**
+     * @param receiveId
+     */
     sendCreateConversation(receiveId)
     {
         this.conn.send(JSON.stringify({
