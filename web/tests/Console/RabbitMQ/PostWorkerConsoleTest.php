e3af363be9330091e770963adc7dae4a2b80ccb7
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-10-12 21:50:20 +0200

    PostWorkerConsole TDD

--- web/tests/Console/RabbitMQ/PostWorkerConsoleTest.php
+++ web/tests/Console/RabbitMQ/PostWorkerConsoleTest.php
@@ -17,6 +17,8 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class PostWorkerConsoleTest extends TestCase
 {
+    use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+
     public function testExecute(): void
     {
         $addPostCommand = Mockery::mock(AddPostCommand::class);
@@ -26,15 +28,15 @@ class PostWorkerConsoleTest extends TestCase
            'post', false, false, false
         ])->once();
         $amqpChannel->shouldReceive('basic_consume')->withArgs([
-            'post', '', false, true, false, false, [AddPostCommand::class, 'execute']
+            'post', '', false, true, false, false, [$addPostCommand, 'execute']
         ])->once();
-        $amqpChannel->shouldReceive('wait')->once();
-        $amqpChannel->shouldReceive('close')->once();
+        $amqpChannel->shouldReceive('wait')->never();
         $amqpChannel->callbacks = [];
 
 
         $rabbitMQConnect = Mockery::mock(RabbitMQConnectService::class);
         $rabbitMQConnect->shouldReceive('getChannel')->once()->andReturn($amqpChannel);
+        $rabbitMQConnect->shouldReceive('close')->once();
 
         $output = Mockery::mock(OutputInterface::class);
         $output->shouldReceive('writeln')->times(2);
