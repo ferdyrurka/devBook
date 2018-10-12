f101a7844b69a85159138e7d2cbd26188ccfbb72
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-10-12 20:32:28 +0200

    ServerDevMessenger TDD

--- web/tests/Console/DevMessenger/ServerDevMessengerTest.php
+++ web/tests/Console/DevMessenger/ServerDevMessengerTest.php
@@ -4,9 +4,38 @@ declare(strict_types=1);
 namespace App\Tests\Console\DevMessenger;
 
 use App\Console\DevMessenger\ServerDevMessenger;
-use PHPUnit\Framework\TestCase;
+use App\Service\DevMessengerService;
+use App\Service\RedisService;
+use Predis\Client;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use \Mockery;
 
-class ServerDevMessengerTest extends TestCase
+/**
+ * Class ServerDevMessengerTest
+ * @package App\Tests\Console\DevMessenger
+ */
+class ServerDevMessengerTest extends KernelTestCase
 {
+    use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 
+    public function testCommand(): void
+    {
+        $client = Mockery::mock(Client::class);
+        $client->shouldReceive('flushdb')->times(2);
+
+        $redisService = Mockery::mock(RedisService::class);
+        $redisService->shouldReceive('setDatabase')->times(2)->with(Mockery::on(function (int $key) {
+            if ($key === 0 || $key === 1) {
+                return true;
+            }
+
+            return false;
+        }))->andReturn($client);
+
+        $devMessengerService = Mockery::mock(DevMessengerService::class);
+
+        $serverDevMessenger = new ServerDevMessenger($devMessengerService, $redisService);
+
+        //No tests execute because it will start a webSocket server
+    }
 }
