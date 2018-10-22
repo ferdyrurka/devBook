d44de7f7ac80e7283c234dc5843bf306b3780794
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-10-22 22:15:44 +0200

    MessagesController TDD added tests UserNotFoundException

--- web/tests/Controller/API/MessagesControllerTest.php
+++ web/tests/Controller/API/MessagesControllerTest.php
@@ -3,9 +3,13 @@ declare(strict_types=1);
 
 namespace App\Tests\Controller\API;
 
+use App\Command\API\GetMessageCommand;
 use App\Controller\API\MessagesController;
+use App\Exception\UserNotFoundException;
+use App\Service\CommandService;
 use App\Tests\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
+use \Mockery;
 
 /**
  * Class MessagesControllerTest
@@ -39,5 +43,19 @@ class MessagesControllerTest extends WebTestCase
             '/api/get-messages/3f5b1ef6-0963-404a-ad48-f3fc730b8e4a'
         );
         $this->assertEquals(Response::HTTP_OK, $this->user->getResponse()->getStatusCode());
+    }
+
+    public function testUserNotFoundException(): void
+    {
+        $messagesController = Mockery::mock(MessagesController::class)->makePartial()
+            ->shouldAllowMockingProtectedMethods();
+        $messagesController->shouldReceive('getUser')->andReturnNull()->once();
+
+        $this->expectException(UserNotFoundException::class);
+        $messagesController->getMessagesAction(
+            Mockery::mock(GetMessageCommand::class),
+            Mockery::mock(CommandService::class),
+            'conversationId'
+        );
     }
 }
