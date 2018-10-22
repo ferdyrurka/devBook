ec16beea1270c54465fc12e6188650ee4ffcea04
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-10-22 21:53:50 +0200

    DevMessengerController UserNotFoundException tests

--- web/tests/Controller/DevMessengerControllerTest.php
+++ web/tests/Controller/DevMessengerControllerTest.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 namespace App\Tests\Controller;
 
 use App\Controller\DevMessengerController;
+use App\Exception\UserNotFoundException;
 use App\Tests\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use \Mockery;
 
 /**
  * Class DevMessengerControllerTest
@@ -13,11 +16,15 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class DevMessengerControllerTest extends WebTestCase
 {
+    use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+
     private $guess;
+    private $user;
 
     public function setUp(): void
     {
         $this->guess = $this->createClientGuess();
+        $this->user = $this->createClientUser();
 
         parent::setUp();
     }
@@ -26,5 +33,24 @@ class DevMessengerControllerTest extends WebTestCase
     {
         $this->guess->request('GET', '/dev-messenger');
         $this->assertEquals(Response::HTTP_FOUND, $this->guess->getResponse()->getStatusCode());
+    }
+
+    public function testDevMessenger(): void
+    {
+        $this->user->request('GET', '/dev-messenger');
+        $this->assertEquals(Response::HTTP_OK, $this->user->getResponse()->getStatusCode());
+    }
+
+    /**
+     * This tests is checking if getUser return null
+     */
+    public function testUserNotFound(): void
+    {
+        $devMessengerController = Mockery::mock(DevMessengerController::class)->makePartial()
+            ->shouldAllowMockingProtectedMethods();
+        $devMessengerController->shouldReceive('getUser')->once()->andReturn(null);
+
+        $this->expectException(UserNotFoundException::class);
+        $devMessengerController->indexAction();
     }
 }
