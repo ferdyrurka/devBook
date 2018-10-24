dc06c2bcefc7693bf81a5444fbda38c597a68b76
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-10-24 15:36:27 +0200

    Added TDD registry user

--- web/tests/Controller/HomeControllerTest.php
+++ web/tests/Controller/HomeControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Controller;
 
+use App\Entity\User;
 use App\Tests\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -31,5 +32,40 @@ class HomeControllerTest extends WebTestCase
     {
         $this->guess->request('GET', '/');
         $this->assertEquals(Response::HTTP_OK, $this->guess->getResponse()->getStatusCode());
+    }
+
+    public function testRegistration(): void
+    {
+        $crawler = $this->guess->request('GET', '/');
+        $this->assertEquals(Response::HTTP_OK, $this->guess->getResponse()->getStatusCode());
+
+        $buttonCrawlerNode = $crawler->selectButton('sign_up_form[RegisterAccount]');
+
+        $formData = [
+            'sign_up_form[username]' => 'admin@lukaszstaniszewski.pl',
+            'sign_up_form[firstName]' => 'FirstNameUser',
+            'sign_up_form[surname]' => 'SurnameUser',
+            'sign_up_form[plainPassword][first]' => 'admin1234',
+            'sign_up_form[plainPassword][second]' => 'admin1234',
+            'sign_up_form[dateBirth][year]' => 1999,
+            'sign_up_form[dateBirth][month]' => 2,
+            'sign_up_form[dateBirth][day]' => 17
+        ];
+
+        $form = $buttonCrawlerNode->form($formData);
+
+        $this->guess->submit($form);
+
+        $em = self::bootKernel();
+        $em = $em->getContainer()->get('doctrine')->getManager();
+
+
+
+        $this->assertNotNull(
+            $user = $em->getRepository(User::class)->findOneBy(['username' => 'admin@lukaszstaniszewski.pl'])
+        );
+
+        $em->remove($user);
+        $em->flush();
     }
 }
