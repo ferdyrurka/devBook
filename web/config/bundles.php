e093179ea57c9db65320d4118d998bade9259a2a
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-10-24 18:26:23 +0200

    Add class in bundles.php

--- web/config/bundles.php
+++ web/config/bundles.php
@@ -12,6 +12,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    FOS\ElasticaBundle\FOSElasticaBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true]
 ];
