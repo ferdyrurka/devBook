bb571eaafdf1c67ebfdbf9bdffe422e6c551c875
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-10-26 23:09:08 +0200

    AddPostToQueueHandler

---
+++ web/src/Handler/API/AddPostToQueueHandler.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Handler\API;
+
+use App\Command\CommandInterface;
+use App\Composite\RabbitMQ\Send\AddPost;
+use App\Composite\RabbitMQ\SendComposite;
+use App\Handler\HandlerInterface;
+
+/**
+ * Class AddPostToQueueCommand
+ * @package App\Command\API
+ */
+class AddPostToQueueHandler implements HandlerInterface
+{
+    /**
+     * @var SendComposite
+     */
+    private $sendComposite;
+
+    /**
+     * AddPostToQueueCommand constructor.
+     * @param SendComposite $sendComposite
+     */
+    public function __construct(SendComposite $sendComposite)
+    {
+        $this->sendComposite = $sendComposite;
+    }
+
+    public function handle(CommandInterface $addPostToQueueCommand): void
+    {
+        $addPost = new AddPost();
+        $addPost->setUserId($addPostToQueueCommand->getUserId());
+        $addPost->setContent(htmlspecialchars($addPostToQueueCommand->getContent()));
+
+        $this->sendComposite->add($addPost);
+        $this->sendComposite->run();
+    }
+}
