15675a44eb4cb4522d6ce78e69535fe987e2c59b
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-10-26 22:13:46 +0200

    Delete business logic in CreateUserCommand

--- web/src/Command/CreateUserCommand.php
+++ web/src/Command/CreateUserCommand.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 namespace App\Command;
 
 use App\Entity\User;
-use App\Entity\UserToken;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * Class CreateUserCommand
@@ -14,73 +11,21 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
  */
 class CreateUserCommand implements CommandInterface
 {
-
-    /**
-     * @var EntityManagerInterface
-     */
-    private $entityManager;
-
-    /**
-     * @var UserPasswordEncoderInterface
-     */
-    private $passwordEncoder;
-
     /**
      * @var User
      */
     private $user;
 
-    /**
-     * CreateUserCommand constructor.
-     * @param EntityManagerInterface $entityManager
-     * @param UserPasswordEncoderInterface $passwordEncoder
-     */
-    public function __construct(EntityManagerInterface $entityManager, UserPasswordEncoderInterface $passwordEncoder)
-    {
-        $this->passwordEncoder = $passwordEncoder;
-        $this->entityManager = $entityManager;
-    }
-
-    /**
-     * @param User $user
-     */
-    public function setUser(User $user): void
+    public function __construct(User $user)
     {
         $this->user = $user;
     }
 
     /**
-     * @throws \Exception
+     * @return User
      */
-    public function execute(): void
+    public function getUser(): User
     {
-        $timeZone = new \DateTimeZone('Europe/Warsaw');
-
-        $time = new \DateTime("now");
-        $time->setTimezone($timeZone);
-
-        $mobileTokenTime = new \DateTime("+10 day");
-        $mobileTokenTime->setTimezone($timeZone);
-
-        $webTokenTime = new \DateTime("+1 day");
-        $webTokenTime->setTimezone($timeZone);
-
-        $publicTokenTime = new \DateTime("+30 day");
-        $publicTokenTime->setTimezone($timeZone);
-
-        $userToken = new UserToken();
-        $userToken->setRefreshMobileToken($mobileTokenTime);
-        $userToken->setRefreshWebToken($webTokenTime);
-        $userToken->setRefreshPublicToken($publicTokenTime);
-
-        $this->user->setCreatedAt($time);
-        $this->user->setRoles('ROLE_USER');
-        $this->user->setStatus(1);
-        $this->user->setPassword($this->passwordEncoder->encodePassword($this->user, $this->user->getPlainPassword()));
-        $this->user->setUserTokenReferences($userToken);
-
-        $this->entityManager->persist($userToken);
-        $this->entityManager->persist($this->user);
-        $this->entityManager->flush();
+        return $this->user;
     }
 }
