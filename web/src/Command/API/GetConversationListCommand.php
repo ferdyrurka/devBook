0d637be9ef6dfe4e3653142c60ec0b64d3d8a6a4
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-10-26 23:30:40 +0200

    GetConversationListCommand delte businness logic

--- web/src/Command/API/GetConversationListCommand.php
+++ web/src/Command/API/GetConversationListCommand.php
@@ -13,35 +13,16 @@ use App\Repository\MessageRepository;
  */
 class GetConversationListCommand implements CommandInterface
 {
-
-    /**
-     * @var MessageRepository
-     */
-    private $messageRepository;
-
-    /**
-     * GetConversationListCommand constructor.
-     * @param MessageRepository $messageRepository
-     */
-    public function __construct(MessageRepository $messageRepository)
-    {
-        $this->messageRepository = $messageRepository;
-    }
-
     /**
      * @var User
      */
     private $user;
 
     /**
-     * @var array
-     */
-    private $result = [];
-
-    /**
+     * GetConversationListCommand constructor.
      * @param User $user
      */
-    public function setUser(User $user): void
+    public function __construct(User $user)
     {
         $this->user = $user;
     }
@@ -49,50 +30,8 @@ class GetConversationListCommand implements CommandInterface
     /**
      * @return User
      */
-    private function getUser(): User
+    public function getUser(): User
     {
         return $this->user;
-    }
-
-    /**
-     * @throws \Doctrine\ORM\NonUniqueResultException
-     */
-    public function execute(): void
-    {
-        $user = $this->getUser();
-        $conversations = $user->getConversationReferences()->getValues();
-
-        $i = 0;
-
-        foreach ($conversations as $conversation) {
-            $conversationId = $conversation->getConversationId();
-            $message = $this->messageRepository->getLastMessageByConversationId($conversationId);
-            $usersConversation = $conversation->getUserReferences()->getValues();
-
-            foreach ($usersConversation as $userConversation) {
-                if ($user->getId() != $userConversation->getId()) {
-                    $this->result[$i]['fullName'] = $userConversation->getFirstName() . ' ' .
-                        $userConversation->getSurname()
-                    ;
-
-                    break;
-                }
-
-                continue;
-            }
-
-            $this->result[$i]['lastMessage'] = $message->getMessage();
-            $this->result[$i]['conversationId'] = $conversationId;
-
-            $i++;
-        }
-    }
-
-    /**
-     * @return array
-     */
-    public function getResult(): array
-    {
-        return $this->result;
     }
 }
