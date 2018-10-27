ef0f4ec572e8515a8a5eef29327e78b74e9ee4fd
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-10-27 21:05:18 +0200

    RegistryUserCommand delete businness logic

--- web/src/Command/Console/DevMessenger/RegistryOnlineUserCommand.php
+++ web/src/Command/Console/DevMessenger/RegistryOnlineUserCommand.php
@@ -4,10 +4,6 @@ declare(strict_types=1);
 namespace App\Command\Console\DevMessenger;
 
 use App\Command\CommandInterface;
-use App\Exception\UserNotFoundException;
-use App\Repository\UserRepository;
-use App\Service\RedisService;
-
 /**
  * Class RegistryOnlineUserCommand
  * @package App\Command\Console\DevMessenger
@@ -15,11 +11,6 @@ use App\Service\RedisService;
 
 class RegistryOnlineUserCommand implements CommandInterface
 {
-    /**
-     * @var RedisService
-     */
-    private $redisService;
-
     /**
      * @var array
      */
@@ -30,111 +21,25 @@ class RegistryOnlineUserCommand implements CommandInterface
      */
     private $connId;
 
-    /**
-     * @var UserRepository
-     */
-    private $userRepository;
-
-    /**
-     * @var bool
-     */
-    private $result = true;
-
-    /**
-     * RegistryOnlineUserCommand constructor.
-     * @param UserRepository $userRepository
-     * @param RedisService $redisService
-     */
-    public function __construct(UserRepository $userRepository, RedisService $redisService)
-    {
-        $this->redisService = $redisService;
-        $this->userRepository = $userRepository;
-    }
-
-    /**
-     * @return int
-     */
-    private function getConnId(): int
-    {
-        return $this->connId;
-    }
-
-    /**
-     * @param int $connId
-     */
-    public function setConnId(int $connId): void
-    {
-        $this->connId = $connId;
-    }
-
-    /**
-     * @param array $message
-     * @return RegistryOnlineUserCommand
-     */
-    public function setMessage(array $message): self
+    public function __construct(array $message, int $connId)
     {
         $this->message = $message;
-
-        return $this;
+        $this->connId = $connId;
     }
 
     /**
      * @return array
      */
-    private function getMessage(): array
+    public function getMessage(): array
     {
         return $this->message;
     }
 
-    public function execute(): void
-    {
-        $userByConn = $this->redisService->setDatabase(0);
-        $message = $this->getMessage();
-
-        $userToken = htmlspecialchars($message['userId']);
-
-        try {
-            $user = $this->userRepository->getOneByPrivateWebToken($userToken);
-        } catch (UserNotFoundException $exception) {
-            $this->result = false;
-            return;
-        }
-
-        /**
-         * Table users by conn id
-         */
-
-        $connId = $this->getConnId();
-
-        if ($userByConn->exists($connId) > 0) {
-            $this->result = false;
-
-            return;
-        }
-
-        $userByConn->set($connId, $userToken);
-
-        /**
-         * Table users by Uuid
-         */
-
-        $userByUuidRedis = $this->redisService->setDatabase(1);
-
-        if ($userByUuidRedis->exists($userToken) > 0) {
-            return;
-        }
-
-        $userByUuidRedis->set($userToken, json_encode([
-            'connId' => $connId,
-            'id' => $user->getId()
-        ]));
-    }
-
     /**
-     * @return bool
+     * @return int
      */
-    public function getResult(): bool
+    public function getConnId(): int
     {
-        return $this->result;
+        return $this->connId;
     }
 }
