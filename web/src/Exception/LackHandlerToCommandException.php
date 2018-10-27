15d5f30051d2428af9fb267a92dc0175542bbb93
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-10-27 22:37:15 +0200

    Added LackHandlerToCommandException

---
+++ web/src/Exception/LackHandlerToCommandException.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+
+namespace App\Exception;
+
+/**
+ * Class LackHandlerToCommand
+ * @package App\Exception
+ */
+class LackHandlerToCommandException extends DevBookException
+{
+}
