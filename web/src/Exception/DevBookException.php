2ab7e3364756eb01b7918158264f7be86022dba3
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-10-27 22:36:20 +0200

    Add DevBookException

---
+++ web/src/Exception/DevBookException.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+
+namespace App\Exception;
+
+/**
+ * Class DevBookException
+ * @package App\Exception
+ * This Exception is application base Exception
+ */
+class DevBookException extends \Exception
+{
+
+}
+
