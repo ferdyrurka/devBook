068bb4be8c0cf39529f121d88e9980a099ccd463
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-10-28 22:17:30 +0100

    NotificationWorkerConsole

---
+++ web/src/Console/RabbitMQ/NotificationWorkerConsole.php
@@ -0,0 +1,64 @@
+<?php
+declare(strict_types=1);
+
+
+namespace App\Console\RabbitMQ;
+
+
+use App\Console\RabbitMQ\Handler\AddNotificationHandler;
+use App\Service\RabbitMQConnectService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class NotificationWorkerConsole extends Command
+{
+    /**
+     * @var AddNotificationHandler
+     */
+    private $addNotificationHandler;
+
+    /**
+     * @var RabbitMQConnectService
+     */
+    private $rabbitMQConnectService;
+
+    public function __construct(
+        AddNotificationHandler $addNotificationHandler,
+        RabbitMQConnectService $rabbitMQConnectService
+    ) {
+        $this->addNotificationHandler = $addNotificationHandler;
+        $this->rabbitMQConnectService = $rabbitMQConnectService;
+
+        parent::__construct();
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $channel = $this->rabbitMQConnectService->getChannel();
+
+        $channel->queue_declare('notification', false, false, false);
+
+        $channel->basic_consume(
+            'notification',
+            '',
+            false,
+            true,
+            false,
+            false,
+            [$this->addNotificationHandler, 'handle']
+        );
+
+        $output->writeln('Notification worker ready to working!');
+
+        while (count($channel->callbacks)) {
+            $channel->wait();
+
+            $output->writeln('I\'m added notification to database!');
+        }
+
+        $output->writeln('Connection closed, bye. :(');
+
+        $this->rabbitMQConnectService->close($channel);
+    }
+}
