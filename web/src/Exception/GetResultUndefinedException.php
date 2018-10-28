e9eab46ac1015f67dee2658b6137a956f9c0b157
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-10-28 10:25:23 +0100

    GetResultUndefinedExpcetion

---
+++ web/src/Exception/GetResultUndefinedException.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+
+namespace App\Exception;
+
+/**
+ * Class GetResultUndefinedException
+ * @package App\Exception
+ */
+class GetResultUndefinedException extends DevBookException
+{
+}
