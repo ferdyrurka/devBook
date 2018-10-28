7edf848e6523c1f5da465f089aa1180b7d31bd3e
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-10-28 14:08:01 +0100

    CreateUserHandler validate Entity before save

--- web/src/Handler/CreateUserHandler.php
+++ web/src/Handler/CreateUserHandler.php
@@ -5,8 +5,10 @@ namespace App\Handler;
 
 use App\Command\CommandInterface;
 use App\Entity\UserToken;
+use App\Exception\ValidateEntityUnsuccessfulException;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
  * Class CreateUserCommand
@@ -26,34 +28,44 @@ class CreateUserHandler implements HandlerInterface
     private $passwordEncoder;
 
     /**
+     * @var ValidatorInterface
+     */
+    private $validator;
+
+    /**
      * CreateUserCommand constructor.
      * @param EntityManagerInterface $entityManager
      * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param ValidatorInterface $validator
      */
-    public function __construct(EntityManagerInterface $entityManager, UserPasswordEncoderInterface $passwordEncoder)
-    {
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        UserPasswordEncoderInterface $passwordEncoder,
+        ValidatorInterface $validator
+    ) {
         $this->passwordEncoder = $passwordEncoder;
         $this->entityManager = $entityManager;
+        $this->validator = $validator;
     }
 
     /**
      * @param CommandInterface $createUserCommand
-     * @throws \Exception
+     * @throws ValidateEntityUnsuccessfulException
      */
     public function handle(CommandInterface $createUserCommand): void
     {
         $timeZone = new \DateTimeZone('Europe/Warsaw');
 
-        $time = new \DateTime("now");
+        $time = new \DateTime('now');
         $time->setTimezone($timeZone);
 
-        $mobileTokenTime = new \DateTime("+10 day");
+        $mobileTokenTime = new \DateTime('+10 day');
         $mobileTokenTime->setTimezone($timeZone);
 
-        $webTokenTime = new \DateTime("+1 day");
+        $webTokenTime = new \DateTime('+1 day');
         $webTokenTime->setTimezone($timeZone);
 
-        $publicTokenTime = new \DateTime("+30 day");
+        $publicTokenTime = new \DateTime('+30 day');
         $publicTokenTime->setTimezone($timeZone);
 
         $userToken = new UserToken();
@@ -68,6 +80,10 @@ class CreateUserHandler implements HandlerInterface
         $user->setStatus(1);
         $user->setPassword($this->passwordEncoder->encodePassword($user, $user->getPlainPassword()));
         $user->setUserTokenReferences($userToken);
+
+        if (\count($this->validator->validate($userToken)) > 0 || \count($this->validator->validate($userToken)) > 0) {
+            throw new ValidateEntityUnsuccessfulException('Failed validate entity in: ' . \get_class($this));
+        }
 
         $this->entityManager->persist($userToken);
         $this->entityManager->persist($user);
