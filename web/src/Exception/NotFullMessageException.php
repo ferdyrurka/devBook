7ab2ad9bd5c333f2c3364dbc7fb559b47d0c0942
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-10-29 15:24:21 +0100

    NotFullMessageException

---
+++ web/src/Exception/NotFullMessageException.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Exception;
+
+/**
+ * Class NotFullMessageException
+ * @package App\Exception
+ */
+class NotFullMessageException extends DevBookException
+{
+}
