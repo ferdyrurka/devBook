54308e1ff97fe8ec5ccaecfbcf0d39be08c62033
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-11-01 20:14:11 +0100

    MessageRepository corrected use namespace

--- web/src/Repository/MessageRepository.php
+++ web/src/Repository/MessageRepository.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace App\Repository;
 
 use App\Entity\Message;
-use App\NullObject\Repository\NullMessageRepository;
+use App\Repository\NullObject\NullMessageRepository;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
 
