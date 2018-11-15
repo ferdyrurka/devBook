b32b83e5254314600292e4a59ad603e06f3994bf
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-11-15 22:28:01 +0100

    SearchFriendsHandler TDD corrected getResult to event

--- web/tests/Handler/API/SearchFriendsHandlerTest.php
+++ web/tests/Handler/API/SearchFriendsHandlerTest.php
@@ -6,10 +6,12 @@ namespace App\Tests\Handler\API;
 use App\Command\API\SearchFriendsCommand;
 use App\Entity\User;
 use App\Entity\UserToken;
+use App\Event\SearchFriendsEvent;
 use App\Handler\API\SearchFriendsHandler;
 use App\Repository\UserRepository;
 use PHPUnit\Framework\TestCase;
 use \Mockery;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
  * Class SearchFriendsCommandTest
@@ -19,7 +21,12 @@ class SearchFriendsHandlerTest extends TestCase
 {
     use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 
-    public function testExecute()
+    /**
+     * @var array
+     */
+    private $result;
+
+    public function testExecute(): void
     {
         $userToken = Mockery::mock(UserToken::class);
         $userToken->shouldReceive('getPublicToken')->once()->andReturn('public_token');
@@ -35,11 +42,26 @@ class SearchFriendsHandlerTest extends TestCase
 
         $searchFriendsCommand = new SearchFriendsCommand(1, '&"<>');
 
-        $searchFriendsHandler = new SearchFriendsHandler($userRepository);
-        $searchFriendsHandler->handle($searchFriendsCommand);
-        $result = $searchFriendsHandler->getResult();
+        $eventDispatcher = Mockery::mock(EventDispatcherInterface::class);
+        $eventDispatcher->shouldReceive('dispatch')
+            ->withArgs(function (string $name, $event) {
+                    if ($event instanceof SearchFriendsEvent) {
+                        $this->result = $event->getUsers();
+                        return true;
+                    }
 
-        $this->assertEquals('FirstName Surname', $result[0]['fullName']);
-        $this->assertEquals('public_token', $result[0]['userId']);
+                    if ($name === SearchFriendsEvent::NAME) {
+                        return true;
+                    }
+
+                    return false;
+                }
+        );
+
+        $searchFriendsHandler = new SearchFriendsHandler($userRepository, $eventDispatcher);
+        $searchFriendsHandler->handle($searchFriendsCommand);
+
+        $this->assertEquals('FirstName Surname', $this->result[0]['fullName']);
+        $this->assertEquals('public_token', $this->result[0]['userId']);
     }
 }
