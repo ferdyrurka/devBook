7466157d6f529da855af19a51e74a9d1860704b7
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-11-16 18:00:15 +0100

    AddMessageEvent

---
+++ web/src/Event/AddMessageEvent.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * Class AddMessageEvent
+ * @package App\Event
+ */
+class AddMessageEvent extends Event
+{
+    /**
+     * @var array
+     */
+    private $sendUsers;
+
+    /**
+     * AddMessageEvent constructor.
+     * @param array $sendUsers
+     */
+    public function __construct(array $sendUsers)
+    {
+        $this->sendUsers = $sendUsers;
+    }
+
+    /**
+     * @return array
+     */
+    public function getSendUsers(): array
+    {
+        return $this->sendUsers;
+    }
+}
